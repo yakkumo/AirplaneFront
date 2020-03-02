668fd54ba55a1f2508d42485d9009fa85b01da96
jhonatan <jhonatan.antunes@vixteam.com.br>
2020-03-02 12:20:37 -0300

    nome do projeto

--- app/e2e/src/app.e2e-spec.ts
+++ app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-httpclient-demo!');
+    expect(page.getTitleText()).toEqual('Welcome to angular-airplane!');
   });
 
   afterEach(async () => {
