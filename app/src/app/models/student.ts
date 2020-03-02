3ca6608e81ed38a2b45fb5b2af87cf9a6e4a7dfb
jhonatan <jhonatan.antunes@vixteam.com.br>
2020-03-02 04:54:10 -0300

    base do crud

---
+++ app/src/app/models/student.ts
@@ -0,0 +1,14 @@
+export class Student {
+    id: number;
+    codigo: string;
+    modelo: string;
+    quantidadePassageiros: number;
+    dataRegistro: Date;
+ }
+/*
+ "codigo": "222",
+ "modelo": "555",
+ "quantidadePassageiros": 888,
+ "dataRegistro": "2019-06-13T08:25:00",
+ "id": 2
+ */
